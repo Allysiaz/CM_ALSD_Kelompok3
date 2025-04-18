052dfe83a7e8253a227880898308141e4c7dedac
maharaaniii <umimaharani08@gmail.com>
2025-04-18 13:53:50 +0700

    bye hara

--- MainMahasiswa.java
+++ MainMahasiswa.java
@@ -1,27 +1,52 @@
 import java.util.Scanner;
-
 public class MainMahasiswa {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
+        Mahasiswa [] daftarMahasiswa = new Mahasiswa[3];
+        MataKuliah [] mk = new MataKuliah[3];
 
-        Mahasiswa[] daftarMahasiswa = {
-                new Mahasiswa("22001", "Ali Rahman", "Informatika"),
-                new Mahasiswa("22002", "Budi Santoso", "Informatika"),
-                new Mahasiswa("22003", "citra Dewi", "Sistem Informasi Bisnis"),
-        };
+        Mahasiswa mhs0 = new Mahasiswa();
+        mhs0.NIM = "22001";
+        mhs0.nama = "Ali Rahman";
+        mhs0.prodi = "Informatika";
+        daftarMahasiswa[0] = mhs0;
 
-        MataKuliah[] mk = {
-                new MataKuliah("MK001", "Struktur Data", 3),
-                new MataKuliah("MK002", "Basis Data", 3),
-                new MataKuliah("MK003", "Desain Web", 3),
-        };
+        Mahasiswa mhs1 = new Mahasiswa();
+        mhs1.NIM = "22002";
+        mhs1.nama = "Budi Santoso";
+        mhs1.prodi = "Informatika";
+        daftarMahasiswa[1] = mhs1;
+
+        Mahasiswa mhs2 = new Mahasiswa();
+        mhs2.NIM = "22003";
+        mhs2.nama = "Citra Dewi";
+        mhs2.prodi = "Sistem Informasi Bisnis";
+        daftarMahasiswa[2] = mhs2;
+
+        MataKuliah mk0 = new MataKuliah();
+        mk0.kodeMK = "MK001";
+        mk0.namaMK = "Struktur Data";
+        mk0.sks    = 3;
+        mk[0] = mk0;
+
+        MataKuliah mk1 = new MataKuliah();
+        mk1.kodeMK = "MK002";
+        mk1.namaMK = "Basis Data";
+        mk1.sks    = 3;
+        mk[1] = mk1;
+
+        MataKuliah mk2 = new MataKuliah();
+        mk2.kodeMK =  "MK003";
+        mk2.namaMK = "Desain Web";
+        mk2.sks    = 3;
+        mk[2] = mk2;
 
         Penilaian[] nilai = {
-                new Penilaian("Ali Rahman", "Struktur Data", 80, 85, 90),
-                new Penilaian("Ali Rahman", "Basis Data", 60, 75, 70),
-                new Penilaian("Budi Santoso", "Struktur Data", 75, 70, 80),
-                new Penilaian("Citra Dewi", "Basis Data", 85, 90, 95),
-                new Penilaian("Citra Dewi", "Desain Web", 80, 90, 65),
+                new Penilaian(mhs0, mk0, 80, 85, 90),
+                new Penilaian(mhs0, mk1, 60, 75, 70),
+                new Penilaian(mhs1, mk0, 75, 70, 80),
+                new Penilaian(mhs2, mk1, 85, 90, 95),
+                new Penilaian(mhs2, mk2, 80, 90, 65),
         };
 
         while (true) {
@@ -45,22 +70,27 @@ public class MainMahasiswa {
                     break;
                 case 2:
                     System.out.println("Daftar Mata Kuliah:");
-                    for (MataKuliah Matkul : mk) {
-                        Matkul.tampilMataKuliah();
+                    for (MataKuliah matakuliah : mk) {
+                        matakuliah.tampilMataKuliah();
                     }
                     break;
                 case 3:
                     System.out.println("Data Penilaian:");
-
+                    for (Penilaian penilaian : nilai) {
+                        penilaian.hitungNilaiAkhir();
+                    }
                     break;
                 case 4:
                     System.out.println("Urutan Mahasiswa berdasarkan Nilai Akhir: ");
+                    double total = 0;
+                    for (Penilaian penilaian : nilai) {
+                        total += penilaian.hitungNilaiAkhir();
+                    }
                     break;
                 case 5:
                     System.out.println("Masukkan NIM Mahasiswa yang dicari:");
                     break;
                 case 0:
-                    System.out.println("Daftar Penilaian:");
                     return;
                 default:
                     System.out.println("Pilihan Tidak Valid!");
--- Penilaian.java
+++ Penilaian.java
@@ -3,19 +3,25 @@ public class Penilaian {
     double nilaiUTS;
     double nilaiUAS;
     double nilaiAkhir;
+    Mahasiswa mahasiswa;
+    MataKuliah mataKuliah;
 
-    Mahasiswa mahasiswa = new Mahasiswa();
-    MataKuliah mk = new MataKuliah();
-
-    public Penilaian(String mahasiswa, String mataKuliah, double tugas, double uts, double uas) {
-
-        mahasiswa = mahasiswa;
+    public Penilaian(Mahasiswa mhs, MataKuliah mataKuliah, double tugas, double uts, double uas) {
+        this.mahasiswa = mhs;
+        this.mataKuliah = mataKuliah;
         nilaiTugas = tugas;
         nilaiUTS = uts;
         nilaiUAS = uas;
     }
 
-    void hitungNilaiAkhir() {
-        System.out.println("gg");
+    public double hitungNilaiAkhir() {
+        nilaiAkhir = 0.3 * nilaiTugas + 0.3 * nilaiUTS + 0.4 * nilaiUAS;
+        System.out.println(mahasiswa.nama + " | " + mataKuliah.namaMK + " | " + " Nilai Akhir :" + nilaiAkhir);
+        return nilaiAkhir;
     }
+
+    void tampilNilai() {
+        System.out.println(mahasiswa.nama + mataKuliah.namaMK + nilaiTugas + nilaiUTS + nilaiUAS);
+    }
+    
 }
